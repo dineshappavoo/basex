87ff752b2ba249c40f46b808f82b422afb574622
dinesgappavoo <dinesha.cit@gmail.com>
2015-06-05 14:35:17 -0700

    Updated basex dict

--- basex.go
+++ basex.go
@@ -1,37 +1,19 @@
+// Package basex generates alpha id (alphanumeric id) for big integers.  This
+// is particularly useful for shortening URLs.
 package basex
-//basex is an go implementation to generate alpha id (alpha numeric id) for big integers. 
-//This will be very helpful to shorten the URL
+
 import (
 	"math/big"
 	"strconv"
 )
 
-var vDICTIONARY_16 []char
-var vDICTIONARY_32 []char
-var vDICTIONARY_62 []char
-var vDICTIONARY_89 []char
-var dictionary []char
+var (
+	dictionary = []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+)
 
-func initialize() {
-	vDICTIONARY_16 = []char{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
-	vDICTIONARY_32 = []char{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
-	vDICTIONARY_62 = []char{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-	vDICTIONARY_89 = []char{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '"', '@', '*', '#', '%', '&', '/', '|', '(', ')', '=', '?', '~', '[', ']', '{', '}', '$', '-', '_', '.', ':', ',', ';', '<', '>'}
-	dictionary = vDICTIONARY_62	
-}
-
-
-//These lines are mandatory to use the char in golang
-type char byte
-func (c char)String()string{
-  return string(c)
-}
-
-//Converts the big integer to alpha id(An alpha numeric id with mixed cases)
-func Encode(val string) string{
-	//Initialize dictionary
-	initialize()
-	var result []char
+// Encode converts the big integer to alpha id (an alphanumeric id with mixed cases)
+func Encode(val string) string {
+	var result []byte
 	var index int
 	var strVal string
 
@@ -47,86 +29,60 @@ func Encode(val string) string{
 	remaining.SetString(val, 10)
 
 	for remaining.Cmp(big.NewInt(0)) != 0 {
-
 		a.Exp(base, big.NewInt(int64(exponent)), nil) //16^1 = 16
-		b := b.Mod(remaining, a)   //119 % 16 = 7 | 112 % 256 = 112
-		c := c.Exp(base, big.NewInt(int64(exponent - 1)), nil)
-		d := d.Div(b, c)
+		b = b.Mod(remaining, a)                       //119 % 16 = 7 | 112 % 256 = 112
+		c = c.Exp(base, big.NewInt(int64(exponent-1)), nil)
+		d = d.Div(b, c)
 
 		//if d > dictionary.length, we have a problem. but BigInteger doesnt have
 		//a greater than method :-(  hope for the best. theoretically, d is always
 		//an index of the dictionary!
 		strVal = d.String()
-		index,_ = strconv.Atoi(strVal)
+		index, _ = strconv.Atoi(strVal)
 		result = append(result, dictionary[index])
 		remaining = remaining.Sub(remaining, b) //119 - 7 = 112 | 112 - 112 = 0
 		exponent = exponent + 1
 	}
 
 	//need to reverse it, since the start of the list contains the least significant values
-	 return reverse(stringVal(result))
+	return string(reverse(result))
 }
 
-//Converts the alpha id to big integer
+// Decode converts the alpha id to big integer
 func Decode(s string) string {
-	//Initialize dictionary
-	initialize()
-    //reverse it, coz its already reversed!
-    chars2 := sliceVal(reverse(s))
+	//reverse it, coz its already reversed!
+	chars2 := reverse([]byte(s))
 
-    //for efficiency, make a map
-    var dictMap map[char]*big.Int
-	dictMap = make(map[char]*big.Int)
+	//for efficiency, make a map
+	dictMap := make(map[byte]*big.Int)
 
-    j := 0
-    for _,val := range dictionary {
-    	dictMap[val] = big.NewInt(int64(j))
-    	j = j+1
-    }
+	j := 0
+	for _, val := range dictionary {
+		dictMap[val] = big.NewInt(int64(j))
+		j = j + 1
+	}
 
-    bi := big.NewInt(0)
+	bi := big.NewInt(0)
 	base := big.NewInt(int64(len(dictionary)))
 
-    exponent := 0;
+	exponent := 0
 	a := big.NewInt(0)
 	b := big.NewInt(0)
 	intermed := big.NewInt(0)
 
-
-    for _,c := range chars2 {
-      a = dictMap[c]
-      intermed = intermed.Exp(base, big.NewInt(int64(exponent)), nil)
-      b = b.Mul(intermed, a)
-      bi = bi.Add(bi, b)
-      exponent = exponent+1
-    }
-    return bi.String()  
-}
-
-func stringVal(s []char) string {
-	var str string
-	for _,val := range s {
-		str = str + string(val)
+	for _, c := range chars2 {
+		a = dictMap[c]
+		intermed = intermed.Exp(base, big.NewInt(int64(exponent)), nil)
+		b = b.Mul(intermed, a)
+		bi = bi.Add(bi, b)
+		exponent = exponent + 1
 	}
-	
-	return str
+	return bi.String()
 }
 
-func sliceVal(s string) []char {
-	var ch char
-	var p []char // == nil
-	for i := 0; i < len(s); i++ {
-		ch = char([]rune(s)[i])
-            p = append(p, ch)
-        }
-    return p
-}
-
-
-func reverse(s string) string {
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+func reverse(bs []byte) []byte {
+	for i, j := 0, len(bs)-1; i < j; i, j = i+1, j-1 {
+		bs[i], bs[j] = bs[j], bs[i]
+	}
+	return bs
 }
